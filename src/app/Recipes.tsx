8da52386c9f4e5fed19f8f7fd867863dacc32bb7
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-09-02 09:21:17 -0700

    Convert home page to a server component

--- src/app/Recipes.tsx
+++ src/app/Recipes.tsx
@@ -1,4 +1,5 @@
-'use client';
+// 'use client';
+// 'use server';
 
 // Next
 import { useRouter } from 'next/navigation';
@@ -20,28 +21,32 @@ import recipesJSON from '../../public/recipes.json';
 
 //------------------------------------------------------------------------------
 type Props = {
-  searchParams: string;
   page: number;
   className?: string;
+  recipeParams: string;
 };
 
 const AMOUNT = 8;
 
-export default function Recipes({ searchParams, page, className }: Props) {
+export default function Recipes({ page, className, recipeParams }: Props) {
   // console.log('Render: Recipes');
 
-  const router = useRouter();
-  const nextSearchParams = useSearchParams();
+  // const router = useRouter();
+  // const nextSearchParams = useSearchParams();
 
-  const { data: recipes = [], isLoading = false } = useQuery({
-    queryKey: ['recipes', searchParams],
-    queryFn: () => API_fetchRecipes(searchParams),
-    staleTime: Infinity,
-    enabled: searchParams !== '',
-  });
+  // const { data: recipes = [], isLoading = false } = useQuery({
+  //   queryKey: ['recipes', searchParams],
+  //   queryFn: () => API_fetchRecipes(searchParams),
+  //   staleTime: Infinity,
+  //   enabled: searchParams !== '',
+  // });
 
-  if (searchParams === '') {
+  console.log('from Recipes, recipeParams:', recipeParams);
+
+  if (recipeParams === '') {
     return <p>Search for some recipes!</p>;
+  } else {
+    return <p>Recipes found!</p>;
   }
 
   // if (recipes.length === 0) {
@@ -52,85 +57,85 @@ export default function Recipes({ searchParams, page, className }: Props) {
   // const isLoading = false;
   // const recipes = recipesJSON['meals'];
 
-  let MAX_PAGES: number | null = null;
-  let startIdx;
-  let endIdx;
-  let paginatedRecipes;
+  // let MAX_PAGES: number | null = null;
+  // let startIdx;
+  // let endIdx;
+  // let paginatedRecipes;
 
-  if (recipes) {
-    MAX_PAGES = Math.ceil(recipes.length / AMOUNT);
+  // if (recipes) {
+  //   MAX_PAGES = Math.ceil(recipes.length / AMOUNT);
 
-    if (page < MAX_PAGES + 1 && page > 0) {
-      startIdx = (page - 1) * AMOUNT;
-      endIdx = page * AMOUNT - 1;
-    }
+  //   if (page < MAX_PAGES + 1 && page > 0) {
+  //     startIdx = (page - 1) * AMOUNT;
+  //     endIdx = page * AMOUNT - 1;
+  //   }
 
-    if (endIdx !== undefined) {
-      paginatedRecipes = recipes?.slice(startIdx, endIdx + 1);
-    }
-  }
+  //   if (endIdx !== undefined) {
+  //     paginatedRecipes = recipes?.slice(startIdx, endIdx + 1);
+  //   }
+  // }
 
-  const handleNavigation = (direction: 'prev' | 'next') => {
-    let currPage = +nextSearchParams.get('page')!;
-    let newPage;
+  // const handleNavigation = (direction: 'prev' | 'next') => {
+  //   let currPage = +nextSearchParams.get('page')!;
+  //   let newPage;
 
-    if (direction === 'prev') {
-      newPage = currPage - 1;
+  //   if (direction === 'prev') {
+  //     newPage = currPage - 1;
 
-      if (newPage <= 0) {
-        return;
-      }
-    }
+  //     if (newPage <= 0) {
+  //       return;
+  //     }
+  //   }
 
-    if (direction === 'next') {
-      newPage = currPage + 1;
+  //   if (direction === 'next') {
+  //     newPage = currPage + 1;
 
-      if (newPage > MAX_PAGES!) {
-        return;
-      }
-    }
+  //     if (newPage > MAX_PAGES!) {
+  //       return;
+  //     }
+  //   }
 
-    router.push(searchParams + `&page=${newPage}`);
-  };
+  //   router.push(searchParams + `&page=${newPage}`);
+  // };
 
-  return (
-    <>
-      {isLoading && <p>Loading recipes...</p>}
-      {!isLoading && !recipes && <p>No recipes found :(</p>}
-      {!isLoading && paginatedRecipes && (
-        <ul
-          className={cn(
-            className,
-            'flex flex-wrap items-start justify-center gap-2',
-          )}
-        >
-          {paginatedRecipes?.map((recipe) => {
-            return (
-              <Recipe
-                key={recipe.idMeal}
-                fetchedRecipe={recipe}
-                params={searchParams}
-              />
-            );
-          })}
-        </ul>
-      )}
-      {!isLoading && MAX_PAGES !== null && recipes.length > AMOUNT && (
-        <div className="mx-4 mt-auto flex justify-between">
-          <Button
-            onClick={() => handleNavigation('prev')}
-            disabled={page === 1}
-          >
-            Prev
-          </Button>
-          <Button
-            onClick={() => handleNavigation('next')}
-            disabled={page === MAX_PAGES}
-          >
-            Next
-          </Button>
-        </div>
-      )}
-    </>
-  );
+  // return (
+  //   <>
+  //     {isLoading && <p>Loading recipes...</p>}
+  //     {!isLoading && !recipes && <p>No recipes found :(</p>}
+  //     {!isLoading && paginatedRecipes && (
+  //       <ul
+  //         className={cn(
+  //           className,
+  //           'flex flex-wrap items-start justify-center gap-2',
+  //         )}
+  //       >
+  //         {paginatedRecipes?.map((recipe) => {
+  //           return (
+  //             <Recipe
+  //               key={recipe.idMeal}
+  //               fetchedRecipe={recipe}
+  //               params={searchParams}
+  //             />
+  //           );
+  //         })}
+  //       </ul>
+  //     )}
+  //     {!isLoading && MAX_PAGES !== null && recipes.length > AMOUNT && (
+  //       <div className="mx-4 mt-auto flex justify-between">
+  //         <Button
+  //           onClick={() => handleNavigation('prev')}
+  //           disabled={page === 1}
+  //         >
+  //           Prev
+  //         </Button>
+  //         <Button
+  //           onClick={() => handleNavigation('next')}
+  //           disabled={page === MAX_PAGES}
+  //         >
+  //           Next
+  //         </Button>
+  //       </div>
+  //     )}
+  //   </>
+  // );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 
 import { useSearchParams } from 'next/navigation';
 
@@ -6,42 +6,51 @@ import Search from './SearchForm';
 import Recipes from './Recipes';
 
 //------------------------------------------------------------------------------
-export default function HomePage() {
+type Props = {
+  searchParams: {
+    i?: string | string[];
+    c?: string;
+    a?: string;
+    page?: string;
+  };
+};
+
+export default function HomePage({ searchParams }: Props) {
   // console.log('Render: Homepage');
 
-  const nextSearchParams = useSearchParams();
+  let ingredients = searchParams.i ?? null;
+  let category = searchParams.c ?? null;
+  let area = searchParams.a ?? null;
 
-  const ingredients = nextSearchParams.getAll('i');
-  const category = nextSearchParams.get('c');
-  const area = nextSearchParams.get('a');
-  let page = nextSearchParams.get('page') ?? '1';
+  let recipeParams = '';
 
-  let searchParams = '';
-
-  if (ingredients.length > 0) {
-    searchParams += '?i=' + ingredients.join('&i=');
+  if (ingredients && ingredients.length > 0) {
+    if (Array.isArray(ingredients)) {
+      recipeParams += '?i=' + ingredients.join('&i=');
+    } else {
+      recipeParams += '?=' + ingredients;
+    }
   } else if (category) {
-    searchParams += '?c=' + category;
+    recipeParams += '?c=' + category;
   } else if (area) {
-    searchParams += '?a=' + area;
+    recipeParams += '?a=' + area;
   }
 
   return (
-    <>
-      <div className="m-4 flex flex-1 flex-col">
-        {/* Search adds the params onto url */}
-        <Search
-          ingredients={ingredients.length > 0 ? ingredients.join(',') : null}
-          category={category}
-          area={area}
-          className="flex flex-col gap-2 md:flex-row md:items-center"
-        />
-        <Recipes
-          searchParams={searchParams}
-          page={+page}
-          className="flex-1-px overflow-auto"
-        />
-      </div>
-    </>
+    <div className="m-4 flex flex-1 flex-col">
+      <Search
+        ingredients={
+          Array.isArray(ingredients) ? ingredients.join(',') : ingredients
+        }
+        category={category}
+        area={area}
+        className="flex flex-col gap-2 md:flex-row md:items-center"
+      />
+      <Recipes
+        recipeParams={recipeParams}
+        page={1}
+        className="flex-1-px overflow-auto"
+      />
+    </div>
   );
 }
