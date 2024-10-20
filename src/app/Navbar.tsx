cf1096c7a13903b4a2ea7052800e108126267b17
thecodeofdaniel <thecodeofdaniel+github@gmail.com>
2024-10-19 19:16:11 -0700

    Add chefhat icon to navbar

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,19 +1,24 @@
 import GlassCard from '../components/GlassCard';
 import Link from 'next/link';
+import Image from 'next/image';
 
 //------------------------------------------------------------------------------
 export default function Navbar() {
   return (
-    <>
-      <nav>
-        <GlassCard className="p-4">
-          <section className="container flex justify-between">
-            <Link href="/" className="text-2xl font-bold">
-              CookMate
-            </Link>
-          </section>
-        </GlassCard>
-      </nav>
-    </>
+    <nav>
+      <GlassCard className="p-4">
+        <section className="container flex justify-between">
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/imgs/chefhat.png" // Adjust this path to where your image is stored
+              alt="Chef Hat"
+              width={30}
+              height={30}
+            />
+            <span className="text-2xl font-bold">CookMate</span>
+          </Link>
+        </section>
+      </GlassCard>
+    </nav>
   );
 }
